e08ba1b37376967983a91b4eb9bb544a3863c1e0
adiprijal <rizalaryan2@gmail.com>
2024-12-20 17:55:37 +0545

    use of the pass statement in loops and control structures

--- 42_pass_statement.py
+++ 42_pass_statement.py
@@ -1,11 +1,22 @@
 # pass is used to continue without doing anything{without executing}
-# function lekhne tara execute nagarne,
+# function lekhne tara execute nagarne
 
-a=20
-if a>=14:
-    pass        # >>> if function doesn't execute
+for a in range (10):
+    if 5<=a<8:
+        pass
+    else:
+        print(a)
 
-while a>14:
-    pass
 
-print(a)
+# to print even numbers
+for b in range (40):
+    if b%2!=0:
+        pass
+    else:
+        print(b)        # >>> prints even numbers from 0 to 39
+
+
+# We use pass statement to write empty loops. Pass is also used for empty control statements, functions, and classes.
+# Pass is a null statement in Python. Nothing happens when it is executed. It is used as a placeholder.
+# We can use pass when we have a block of code that is not implemented, but we want to implement it in the future.
+# It is used to prevent indentation errors and indicates that no action is to be taken.
