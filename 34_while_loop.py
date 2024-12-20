4ade0125a95eef17c058766f2316a49f428d08ab
adiprijal <rizalaryan2@gmail.com>
2024-12-20 17:49:12 +0545

    while loop

--- 34_while_loop.py
+++ 34_while_loop.py
@@ -9,3 +9,19 @@ while a<10:
 # while a<=1000:
 #     print(a)
 #     a=a+1             >>> prints 0 to 1000
+
+
+while True:
+    print('Enter a number')
+    num = int(input())
+    if num>100:
+        print('Congrats you have entered a number greater than 100')
+        break
+
+''' 
+    >> Infinite loop <<
+
+while True:
+    print('Yes')        # >>> infinite loop
+
+'''
